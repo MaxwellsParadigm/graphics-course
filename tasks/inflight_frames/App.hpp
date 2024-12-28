1086ed52e43f2d200ba97ce4a3a545ed1c3af41f
MaxwellsParadigm <102373296+MaxwellsParadigm@users.noreply.github.com>
2024-12-28 18:53:13 +0300

    Update App.hpp

--- tasks/inflight_frames/App.hpp
+++ tasks/inflight_frames/App.hpp
@@ -1,16 +1,14 @@
 #pragma once
 
-#include <etna/Window.hpp>
-#include <etna/PerFrameCmdMgr.hpp>
-#include <etna/ComputePipeline.hpp>
-#include <etna/Image.hpp>
-#include <etna/Sampler.hpp>
-#include <chrono>
-
-
+#include "etna/Buffer.hpp"
 #include "etna/GraphicsPipeline.hpp"
 #include "wsi/OsWindowingManager.hpp"
-
+#include <chrono>
+#include <etna/ComputePipeline.hpp>
+#include <etna/Image.hpp>
+#include <etna/PerFrameCmdMgr.hpp>
+#include <etna/Sampler.hpp>
+#include <etna/Window.hpp>
 
 class App
 {
@@ -20,39 +18,34 @@ public:
 
   void run();
 
+  static constexpr size_t NUM_FRAMES_IN_FLIGHT = 2;
+
 private:
   void drawFrame();
 
 private:
-  OsWindowingManager windowing;
-  std::unique_ptr<OsWindow> osWindow;
-
-  glm::uvec2 resolution;
-  bool useVsync;
-
-  std::unique_ptr<etna::Window> vkWindow;
-  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
-
-  etna::Image generatedTextureImage;
-  etna::Image loadedTextureImage1;
-
-  etna::Sampler defaultSampler;
-
-  etna::GraphicsPipeline graphicsPipeline;
-  etna::GraphicsPipeline texturePipeline;
-
-  std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
-  glm::vec2 mouse_pos;
-
-  std::vector<etna::Buffer> constantsBuffers;
-  int current_frame;
-
-  bool is_textures_loaded = false;
-
-  struct PushConstants
+  struct Params
   {
     float time;
     glm::vec2 resolution;
     glm::vec2 mouse_pos;
   };
+
+  OsWindowingManager windowing;
+  std::unique_ptr<OsWindow> osWindow;
+  std::unique_ptr<etna::Window> vkWindow;
+  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+
+  glm::uvec2 resolution;
+  etna::Image Texture;
+  etna::Image Image1;
+
+  etna::Sampler defaultSampler;
+  etna::GraphicsPipeline graphicsPipeline;
+  etna::GraphicsPipeline texturePipeline;
+  bool is_textures_loaded = false;
+  std::chrono::time_point<std::chrono::system_clock> start_time = std::chrono::system_clock::now();
+
+  int frame_num = 0;
+  std::array<etna::Buffer, 2> FrameBuffer;
 };
